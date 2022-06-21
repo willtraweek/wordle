ddcd726e930adb2dde55ebf65f3b0dd18da24248
Will Traweek <will.traweek@gmail.com>
2022-06-20 20:25:39 -0500

    add get(/word/:guess)

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,10 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller,
+  HttpException, HttpStatus,
+  Get,
+  Param } from '@nestjs/common';
 import { AppService } from './app.service';
+import {Letter} from "./utilities/Letter";
+import {guessResponse} from "./utilities/word";
 
 @Controller()
 export class AppController {
@@ -9,5 +14,18 @@ export class AppController {
   getWord(): string {
     //resets the game by deleting the current word
     return this.appService.getWord();
+  }
+
+  @Get("/word/:guess")
+  checkGuess(@Param("guess") guess: string): object {
+    // checks to see what letters the guess got correct
+    try {
+      return this.appService.checkGuess(guess);
+    } catch (e) {
+      throw new HttpException({
+        status: HttpStatus.BAD_REQUEST,
+        error: e.message
+      }, HttpStatus.BAD_REQUEST);
+    }
   }
 }
