2305a13fa16fe3bf03bed727e04a9185565d05a0
Will Traweek <will.traweek@gmail.com>
2022-06-20 19:21:16 -0500

    add getWord

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -5,8 +5,9 @@ import { AppService } from './app.service';
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Get("/word")
+  getWord(): string {
+    //resets the game by deleting the current word
+    return this.appService.getWord();
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@nestjs/common';
+import { Word } from "./utilities/word";
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  getWord(): string {
+    return new Word().word;
   }
 }
